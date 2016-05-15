3e7fa3b3ecdf006acba1334e481fb7f29d2d9094
edubernstein <SarahB1976>
2016-05-15 13:46:59 -0300

    Usando Embedded

--- jpa/src/main/java/br/com/k19/jpa/RemoveEditoraComJPA.java
+++ jpa/src/main/java/br/com/k19/jpa/RemoveEditoraComJPA.java
@@ -9,18 +9,23 @@ import javax.persistence.Persistence;
 public class RemoveEditoraComJPA {
 	
 	public static void main(String[] args) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("livraria-pu");
-		EntityManager manager = factory.createEntityManager();
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Digite o id da editora que deseja remover: ");
-		Long id = Long.parseLong(entrada.nextLine());
-		entrada.close();
-		Editora e = manager.find(Editora.class, id);
-		manager.remove(e);
-		manager.getTransaction().begin();
-		manager.getTransaction().commit();
-		manager.close();
-		factory.close();
+		try {
+			EntityManagerFactory factory = Persistence.createEntityManagerFactory("livraria-pu");
+			EntityManager manager = factory.createEntityManager();
+			Scanner entrada = new Scanner(System.in);
+			System.out.println("Digite o id da editora que deseja remover: ");
+			Long id = Long.parseLong(entrada.nextLine());
+			entrada.close();
+			Editora e = manager.find(Editora.class, id);
+			manager.remove(e);
+			manager.getTransaction().begin();
+			manager.getTransaction().commit();
+			manager.close();
+			factory.close();
+			System.out.println(e.getNome());
+		} catch (Exception e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
 	
 }
